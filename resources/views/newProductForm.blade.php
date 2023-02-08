f30a6c7e8ea5b8ceee9fe86e03c5b7b98165b873
rodri-afa <rodrigo.afaformacion@gmail.com>
2023-02-08 14:34:08 +0100

    🔥

---
+++ resources/views/newProductForm.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="sfdgdhsyk" method="post">
+        @csrf
+        <input type="text" name="nombre">
+        <input type="number" name="precio">
+        <input type="text" name="descripcion">
+        <input type="number" name="stock">
+        <input type="submit" value="nuevo producto">
+    </form>
+</body>
+</html>
